3df5009902247d03db4981c7abb33e9f656c98d1
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-07-19 14:37:55 +0200

    Add AboutPage component with introductory content

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About Us</h1>
+      <p>
+        Welcome to Watchdog Media. Learn more about our mission and team here.
+      </p>
+    </main>
+  );
+}
