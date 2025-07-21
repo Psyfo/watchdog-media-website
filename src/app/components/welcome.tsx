d11b14991ee795d0746c082feda2e90d1dc40caf
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-07-21 12:09:56 +0200

    Refactor Welcome component to use TextBlock for heading and description

--- src/app/components/welcome.tsx
+++ src/app/components/welcome.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
+import TextBlock from '@/components/text-block';
 
 const Welcome: React.FC = () => {
   return (
     <section className='flex flex-col items-center justify-center px-6 py-[80px] text-center md:py-[120px] bg-watchdog-light'>
-      <h1 className='mb-6 text-[18px] font-bold text-watchdog-dark font-noteworthy md:text-[30px] uppercase'>
-        Welcome to Watchdog Media
-      </h1>
-      <p className='md:max-w-[50vw] text-base leading-relaxed text-watchdog-dark font-myriad md:text-[30px] text-justify'>
-        Film production support services for local and international film
-        productions seeking to film in Durban and other parts of KwaZulu- Natal,
-        South Africa.
-      </p>
+      <TextBlock
+        heading='Welcome to Watchdog Media'
+        text='Film production support services for local and international film productions seeking to film in Durban and other parts of KwaZulu-Natal, South Africa.'
+      />
     </section>
   );
 };
