5d1353ca9030eddac0f2975285df0336845fe168
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-07-21 14:27:54 +0200

    Add Awards component to display recognition and accolades for Watchdog Media

---
+++ src/app/components/awards.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TextBlock from '@/components/text-block';
+
+const Awards: React.FC = () => {
+  return (
+    <section className='flex flex-col items-center px-6 py-16 text-center bg-white md:py-24'>
+      <TextBlock
+        heading='Awards'
+        text='Watchdog Media has been recognized for its excellence in film production, receiving numerous awards and accolades.'
+      />
+    </section>
+  );
+};
+
+export default Awards;
