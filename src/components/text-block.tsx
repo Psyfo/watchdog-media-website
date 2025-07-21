4ec747438cf9e11d3484654ad1f731b7effc5683
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-07-21 14:28:30 +0200

    Enhance TextBlock component with animation support using Framer Motion

--- src/components/text-block.tsx
+++ src/components/text-block.tsx
@@ -1,21 +1,42 @@
-import React from 'react';
+'use client';
+import React, { useRef } from 'react';
+import { Variants, motion, useInView } from 'framer-motion';
 
 interface TextBlockProps {
   heading?: string;
   text?: string;
 }
 
-const TextBlock: React.FC<TextBlockProps> = ({ heading, text }) => (
-  <div>
-    {heading && (
-      <h1 className='mb-6 text-[18px] font-bold text-watchdog-dark font-noteworthy md:text-[30px] uppercase'>
-        {heading}
-      </h1>
-    )}
-    <p className='md:max-w-[50vw] text-base leading-relaxed text-watchdog-dark font-myriad md:text-[30px] text-justify'>
-      {text}
-    </p>
-  </div>
-);
+const textVariants: Variants = {
+  hidden: { opacity: 0, y: 50 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.8, ease: 'easeOut' },
+  },
+};
+
+const TextBlock: React.FC<TextBlockProps> = ({ heading, text }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, margin: '-100px' });
+
+  return (
+    <motion.div
+      ref={ref}
+      variants={textVariants}
+      initial='hidden'
+      animate={isInView ? 'visible' : 'hidden'}
+    >
+      {heading && (
+        <h1 className='mb-6 text-[18px] font-bold text-watchdog-dark font-noteworthy md:text-[30px] uppercase'>
+          {heading}
+        </h1>
+      )}
+      <p className='md:max-w-[50vw] text-base leading-relaxed text-watchdog-dark font-myriad md:text-[30px] text-justify'>
+        {text}
+      </p>
+    </motion.div>
+  );
+};
 
 export default TextBlock;
