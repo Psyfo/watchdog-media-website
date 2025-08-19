6d476bb214437553402ad537abdb3a9274f19c91
Sipho Mahlangu <21422869@dut4life.ac.za>
2025-08-19 16:56:32 +0200

    feat: add ProductionItem component for animated display of production items

--- src/components/ProductionItem.tsx
+++ src/components/ProductionItem.tsx
@@ -1,6 +1,6 @@
 'use client';
-import React, { useRef } from 'react';
 import { motion, useInView } from 'framer-motion';
+import React, { useRef } from 'react';
 
 interface ProductionItemProps {
   name: string;
